fcd70420d828797fbcee56c703b5170329496872
Sahil Khatri <149064797+SAHILKHATRI11@users.noreply.github.com>
2025-02-15 23:41:12 +0530

    Time: 24 ms (17.31%) | Memory: 69 MB (53.20%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,39 +1,35 @@
 class ProductOfNumbers {
-
-    // Stores cumulative product of the stream
-    private ArrayList<Integer> prefixProduct = new ArrayList<>();
-    private int size = 0;
-
+    ArrayList<Integer> list;
+    int size ;
     public ProductOfNumbers() {
-        // Initialize the product list with 1 to handle multiplication logic
-        this.prefixProduct.add(1);
-        this.size = 0;
+        list = new ArrayList<>();
+        list.add(1);
+        size = 0;
     }
-
+    
     public void add(int num) {
-        if (num == 0) {
-            // If num is 0, reset the cumulative products since multiplication
-            // with 0 invalidates previous products
-            this.prefixProduct = new ArrayList<Integer>();
-            this.prefixProduct.add(1);
+        if(num == 0){
+            this.list = new ArrayList<Integer>();
+            this.list.add(1);
             this.size = 0;
-        } else {
-            // Append the cumulative product of the current number with the last
-            // product
-            this.prefixProduct.add(this.prefixProduct.get(size) * num);
-            this.size++;
+            }
+            else{
+                this.list.add(this.list.get(size)*num);
+                this.size++;
+            }
         }
-    }
 
     public int getProduct(int k) {
-        // Check if the requested product length exceeds the size of the valid
-        // product list
-        if (k > this.size) return 0;
-
-        // Compute the product of the last k elements using division
-        return (
-            this.prefixProduct.get(this.size) /
-            this.prefixProduct.get(this.size - k)
-        );
+        if(this.size < k)return 0;
+        int a = list.get(list.size()-1);
+        int b = list.get(list.size()-k-1);
+        return a/b;
     }
 }
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
