45bf6e6a4fe5b8d717b97ee5bfc5f949a2960fc5
Sahil Khatri <149064797+SAHILKHATRI11@users.noreply.github.com>
2025-04-29 09:38:50 +0530

    Time: 1 ms (56.95%) | Memory: 61.5 MB (32.82%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -1,11 +1,12 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-       int i = 0;
-       if(nums.length <= 2)return nums.length;
-       while(Math.pow(2,i) < nums.length){
-        i++;
-       }
-       if(nums.length % (int)Math.pow(2,i) == 0) return (int)Math.pow(2,i+1);
-       return (int)Math.pow(2,i);
+        int ans = nums.length;
+        if(ans < 3)return ans;
+        int count = 0;
+        while(ans > 0){
+            ans >>= 1;
+            count++;
+        }
+        return (int)Math.pow(2,count);
     }
 }
